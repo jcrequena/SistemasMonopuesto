9f4936c303a5d279cb43e2b2db047caf497f1428
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-02-19 16:48:02 +0100

    Update head.bat

--- UD5/comandos/head.bat
+++ UD5/comandos/head.bat
@@ -1,14 +1,16 @@
 ::Uso head fichero n-lineas
-::%1 es el nombre del fichero y %2 las líneas a visualizar
+::En la variable %1 se guarda el 1er parámetro (nombre del fichero) y %2 se guarda el 2º (las líneas a visualizar)
 @echo off
 
 setlocal enabledelayedexpansion
+::La variable num se utiliza para contar las lineas que se han leído en el bucle for
 set num=0
-REM La variable num se utiliza para ennumerar las lineas
-
-:: El propósito de este FOR, es leer las lineas de "texto.txt" e ir almacenándolas en una variable numerada
+:: El propósito de este FOR, es leer las lineas del fichero que se pasa por parámetro
+:: e ir sacando por pantalla las mismas hasta llegar al valor n-lineas (%2 parámetro 2)
 for /f "tokens=*" %%h in (%1) do (
     set /a num=!num!+1
     set LIN_!num!=%%h
     echo %%h
+    IF !num!==%2 (exit /b)
+    
 )
