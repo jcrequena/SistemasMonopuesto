a1a5acfc5500763d4cbfb1f9929423b883cf686c
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-03-05 19:24:38 +0100

    Update cut.bat

--- UD5/comandos/cut.bat
+++ UD5/comandos/cut.bat
@@ -1,3 +1,15 @@
 @echo off
-REM recuperamos las columna 1, 5 y 7
-(for /f "tokens=1,5,7 delims=:" %%a in (input.txt) do echo %%a-%%b-%%c) > output.txt
+:: La llamada al script, ha de ser la siguiente:
+:: cut.bat fichero_origen 
+:: donde %1 es el primer parámetro 
+:: ejemplo cut.bat origen.txt
+:: Para este caso de ejemplo, recuperamos las columnas 1, 5 y 7
+::
+if "%1"=="" (echo "Falta el primer parámetro. Uso: cut.bat fichero_a_leer" 
+	      exit /b
+            )
+
+(for /f "tokens=1,5,7 delims=:" %%a in (%1) do echo %%a-%%b-%%c)
+
+::Referencias
+::https://gist.github.com/HiroNakamura/4702728
