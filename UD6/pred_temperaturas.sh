b49500f16e4c9a6b5c2caefc4d46f566ceb31bda
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-04-08 11:11:54 +0200

    Create pred_temperaturas.sh

---
+++ UD6/pred_temperaturas.sh
@@ -0,0 +1,10 @@
+
+#En la variable dia, llevamos la cuenta de los pŕoximo 24, 48 y 72 horas, es decir, 1, 2 o 3.
+dia=1
+#Bucle donde en cada pasada,horas vale 24, 48 o 72.
+for horas in $( seq 24 48 72); do
+  #Componer el fichero png a descargar
+  ruta=
+  wget http://www.aemet.es/imagenes_d/eltiempo/prediccion/temperaturas/$ruta -O /home/$USER/inm/temperaturas-max2019
+  dia=$((dia+1))
+done
