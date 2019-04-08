2111f2ce2d186bec92aab3b91f7c8d4c80aa8841
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-04-08 10:56:59 +0200

    Create estructuraINM.sh

---
+++ UD6/estructuraINM.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#By: Juan Carlos Requena
+#Versión 1.0
+
+#Varibles Globales
+zero=0
+#Fin declaración de varibales globales
+
+#Creamos la estructura de directorio para las imágenes Rádar
+#/home/smx1/inm
+#├── radar2019
+#│   ├── 01
+#│   │   ├── 01
+#│   │   ├── 02
+#│   │   ├── 03
+#....
+
+#El primer bucle recorre cada uno de los 12 mese del año, y el segundo bucle,
+# va creando los directorios para cada uno de los días por mes
+
+for  mes in $( seq 1 1 12 ); do
+    if [ $mes -lt 10 ]; then
+		  month=$zero$mes
+    else
+		  month=$mes
+    fi
+    #Creamos los directorios para los días para las imágenes Rádar	
+    for dia in $(seq 1 1 31 ); do
+        if [ $dia -lt 10 ]; then
+		    day=$zero$dia
+	    else
+		    day=$dia
+	    fi
+	    mkdir -p /home/$USER/inm/radar2019/$month/$day
+    done
+done
+	
