f257aa66aa799c0171076b331578447786b14d5a
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-05-24 15:44:03 +0200

    Update Menu_Linux.sh

--- UD7/proyecto/Menu_Linux.sh
+++ UD7/proyecto/Menu_Linux.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#Script By: Autor/a
+#Autor/a: JC Requena
+#Fecha 24/05/2019
+#Descripción: Menú básico con 4 opciones y una final para salir del script.
+#Cada opcióndel menú, accede a una función que es donde se desarrolla una funcionalidad en concreto.
 
 #     
 # Declaración de FUNCIONES
@@ -31,9 +34,9 @@ do
 	clear
 	echo "Menu" 
 	echo "_________________________________________"
-  echo ""
-  echo "1)" "Opcion1"
-  echo "2)" "Opcion2"
+  	echo ""
+  	echo "1)" "Opcion1"
+  	echo "2)" "Opcion2"
 	echo "3)" "Opcion3"
 	echo "4)" "SALIR"
 	echo "_________________________________________"
@@ -43,25 +46,25 @@ do
 	#Comprueba si el valor recogido en opcion es 1,2,3 o 4, si es otra cosa, se ejecuta *)
     	case $opcion in
     	1) 
-			    fntFuncion1
-			    read -p "Press [Enter] key to continue..."
+		fntFuncion1
+		read -p "Press [Enter] key to continue..."
        		;;
     	2) 
-			     fntFuncion2
-			     read -p "Press [Enter] key to continue..."
+		fntFuncion2
+		read -p "Press [Enter] key to continue..."
        		;;
     	3) 
-			    fntFuncion3
-			    read -p "Press [Enter] key to continue..."
+		fntFuncion3
+		read -p "Press [Enter] key to continue..."
         	;;
     	4) 
-		      clear
+		clear
         	#Con exit salimos del script
           exit
         	;;
     	*) 
-		    echo "Error: Seleccione una opcion valida [1-4]!"
-        read -p "Press [Enter] key to continue..."
+		echo "Error: Seleccione una opcion valida [1-4]!"
+        	read -p "Press [Enter] key to continue..."
 		;;
    	esac
 done
