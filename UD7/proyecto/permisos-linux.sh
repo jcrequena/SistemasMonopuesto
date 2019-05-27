ad1530ae6a300b394e0059312b0214ef25a446b1
Juan Carlos <jrequenav@iesmiralcamp.org>
2019-05-27 17:37:52 +0200

    Update permisos-linux.sh

--- UD7/proyecto/permisos-linux.sh
+++ UD7/proyecto/permisos-linux.sh
@@ -4,18 +4,26 @@ mkdir /pub/Documentación/Direccion
 mkdir /pub/Documentación/Informatica
 
 
-# Se han creado los grupos Direccion_RW e Informatica_RW
-# Se han añadido los usuarios a los grupos
-# Se Aplican los permisos
-
+# Se crean los grupos Direccion_RW e Informatica_RW
 groupadd Direccion_RW 
-#Añadir el usaurio usuario_Direcc al grupo Dirección_RW
+groupadd Informatica_RW 
+#Se añade el usaurio usuario_Direcc al grupo Dirección_RW
 adduser usuario_Direcc Dirección_RW 
+#Se añade el usaurio usuario_Direcc al grupo Dirección_RW
+adduser usuario_Inf Informatica_RW 
 
+# Aplicamos los permisos, para que los usuarios de Dirección puedan rwx en el directorio Direccion
+# y los usuarios de Dirección puedan rwx en el directorio Informatica
 #Cambiamos el propietario del grupo al directorio Direccion. El nuevo propietario es Direccion_RW
 chgrp /pub/Documentación/Direccion Direccion_RW
 #Establecemos los permisos de rwx al grupo propietario del directorio Direccion
 chmod g+rwx /pub/Documentación/Direccion
+
+#Cambiamos el propietario del grupo al directorio Direccion. El nuevo propietario es Direccion_RW
+chgrp /pub/Documentación/Informatica Informatica_RW
+#Establecemos los permisos de rwx al grupo propietario del directorio Direccion
+chmod g+rwx /pub/Documentación/Informatica
+
 
 
 
