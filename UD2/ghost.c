5b1fd67ef62d83c3293afaa2e101095f416a4cf8
Juan Carlos <jcrequena.fp@gmail.com>
2019-11-09 11:55:47 +0100

    Update ghost.c

--- UD2/ghost.c
+++ UD2/ghost.c
@@ -8,26 +8,25 @@ long int ma2[FIL][FIL];
 
 int main(void)
 {	
-	int fil,col,k;
-	long long **des;
-    int sal=0;
-    long ext =0;
-	while (1)
-	{
-        
-        for ( fil = 0; fil < FIL; fil ++ )
-	    {
-		    for ( col = 0; col < FIL; col ++ )
-		    {	
-			    ma1[fil][col] = 1 + fil + col;
-			    sleep(0.1);
-                	    printf("Matriz [%d][%d] = %lu\n",fil,col,ma1[fil][col]);
-		    }
-	    }
-	    for ( sal = 0; sal < 10000; sal ++ )
-        {
-            printf("Ciclos ghost --> monitor\n");
-        }
-	}
-	return 0;
+int fil,col,k;
+long long **des;
+int sal=0;
+long ext =0;
+while (1)
+{  
+  for ( fil = 0; fil < FIL; fil ++ )
+  {
+    for ( col = 0; col < FIL; col ++ )
+    {	
+	ma1[fil][col] = 1 + fil + col;
+	sleep(0.1);
+        printf("Matriz [%d][%d] = %lu\n",fil,col,ma1[fil][col]);
+     }
+  }
+  for ( sal = 0; sal < 10000; sal ++ )
+  {
+      printf("Ciclos ghost --> monitor\n");
+   }
+}
+ return 0;
 }
