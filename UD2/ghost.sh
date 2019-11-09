46770bb0a86ab195709556a2f7869de6c7e58132
Juan Carlos <jcrequena.fp@gmail.com>
2019-11-09 11:15:22 +0100

    Update ghost.sh

--- UD2/ghost.sh
+++ UD2/ghost.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-for i in `seq  1  3`
-do 
-  ./ghost &
+#! /bin/bash
+# Script que ejecuta 4 veces el binario ghost en segundo plano
+cnt=0
+while [  $cnt -lt 5 ]; do
+	./ghost &
+	let cnt=cnt+1 
 done
