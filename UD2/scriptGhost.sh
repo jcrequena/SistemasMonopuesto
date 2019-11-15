046427c414eed76f068eccf86c2651b26cf8bd63
Juan Carlos <jcrequena.fp@gmail.com>
2019-11-15 15:42:30 +0100

    Update scriptGhost.sh

--- UD2/scriptGhost.sh
+++ UD2/scriptGhost.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Script que ejecuta 4 veces el binario ghost en segundo plano
+# Script que ejecuta 5 veces el binario ghost en segundo plano
 cnt=0
 while [  $cnt -lt 5 ]; do
 	./ghost &
