e890109743f1aa3ab7f6c39933d349e405064ec2
Juan Carlos <jcrequena.fp@gmail.com>
2020-03-25 15:52:33 +0100

    Update Menu_Linux.sh

--- UD6/Linux/Menu_Linux.sh
+++ UD6/Linux/Menu_Linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script By: Autor/a
+#Script By: JCRequena
 
 #     
 # Declaración de FUNCIONES
@@ -10,12 +10,27 @@ fntFuncion1() {
 }  #Fin función fntFuncion1
 	
 fntFuncion2() {
-  echo "Estoy en la función 2"
+  	echo "Estoy en la función 2"
 } #Fin función fntFuncion2
 
 fntFuncion3() {
-  echo "Estoy en la función 3"
+  	echo "Estoy en la función 3"
 } #Fin función fntFuncion3
+
+fntmostrarMenu () {
+    	clear
+    	echo $USERNAME
+    	echo ""
+	echo "Menu" 
+	echo "_________________________________________"
+  	echo ""
+  	echo "1)" "Opcion1"
+  	echo "2)" "Opcion2"
+	echo "3)" "Opcion3"
+	echo "4)" "SALIR"
+	echo "_________________________________________"
+	echo ""
+}
 
 
                                            ####################
@@ -30,42 +45,29 @@ fntFuncion3() {
 #
 while [ opcion != "" ]
 do
-	clear
-	echo ""
-	echo "Menu" 
-	echo "_________________________________________"
-  echo ""
-  echo "1)" "Opcion1"
-  echo "2)" "Opcion2"
-	echo "3)" "Opcion3"
-	echo "4)" "SALIR"
-	echo "_________________________________________"
-	echo ""
-
+	
+	fntmostrarMenu
 	read -p "Seleccione la operacion a realizar [1-4]: " opcion
 	#Comprueba si el valor recogido en opcion es 1,2,3 o 4, si es otra cosa, se ejecuta *)
     	case $opcion in
     	1) 
-			    fntFuncion1
-			    read -p "Press [Enter] key to continue..."
+		fntFuncion1
        		;;
     	2) 
-			     fntFuncion2
-			     read -p "Press [Enter] key to continue..."
+		fntFuncion2	   
        		;;
     	3) 
-			    fntFuncion3
-			    read -p "Press [Enter] key to continue..."
+		fntFuncion3
         	;;
     	4) 
 		clear
-        	#Con exit salimos del script
-          exit
+		echo "Saliendo de la App....."
+		#Con exit salimos del script
+          	exit
         	;;
     	*) 
 		echo "Error: Seleccione una opcion valida [1-4]!"
-        read -p "Press [Enter] key to continue..."
 		;;
    	esac
+	read -p "Press [Enter] key to continue..."
 done
-© 2020 GitHub, Inc.
