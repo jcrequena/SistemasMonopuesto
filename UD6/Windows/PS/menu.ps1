7d035e70b08550425d71d802022d56072dbd233b
Juan Carlos <jcrequena.fp@gmail.com>
2020-03-25 15:46:15 +0100

    Update menu.ps1

--- UD6/Windows/PS/menu.ps1
+++ UD6/Windows/PS/menu.ps1
@@ -1,11 +1,29 @@
 #Autor:JCRequena
 #Lo que se pone a partir de la almohadilla(#) es un comentario y no se interpreta por el compilador. No es código fuente.
 #
+function fnt_Opcion-1 
+{
+	cls 
+        'Has pulsado la opcion 1' 
+}
+function fnt_Opcion-2
+{
+	cls 
+        'Has pulsado la opcion 2' 
+}
+function fnt_Opcion-3 
+{
+	cls 
+        'Has pulsado la opcion 3' 
+}
+
 function mostrarMenu 
 { 
      cls 
-     Write-Host "===============Menu Principal================" 
-      
+     'Bienvenido' $env:USERNAME
+     Write-Host "============================================" 
+     Write-Host "===============Menu Principal===============" 
+     Write-Host "============================================" 
      Write-Host "1) Opcion 1" 
      Write-Host "2) Opcion 2" 
      Write-Host "3) Opcion 3" 
@@ -20,21 +38,28 @@ do
      #Referencia:https://ss64.com/ps/switch.html
      switch ($input) 
      { 
-           '1' { 
-                cls 
-                'Has pulsado la opcion 1' 
-           } '2' { 
-                cls 
-                'Has pulsado la opcion 2' 
-           } '3' { 
-                cls 
-                'Has pulsado la opcion 3' 
-           } 's' { 
-                return 
-           } default {
-		'Has de pulsar un numero entre 1-3 o pulsar s'
+           '1' 
+	   { 
+                fnt_Opcion-1 
+           } 
+	   '2' 
+	   { 
+                fnt_Opcion-2
+           } 
+	   '3' 
+	   { 
+                fnt_Opcion-3 
+           } 
+	   's' 
+	   { 
+               	#Con return nos salimos del script 
+		return 
+           } 
+	   default 
+	   {
+		'Has de pulsar un numero entre 1-3 o pulsar S'
            }
-	     
      } 
      pause 
-} until ($input -eq 's')
+} until ($input -eq 's' -or $input -eq 'S')
+#Se comprueba si se ha pulsado la s minúscula o mayúscula (S).
