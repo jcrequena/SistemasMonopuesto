c74854a8e995f02c1932874bc3197e4abf58b83e
Juan Carlos <jcrequena.fp@gmail.com>
2020-04-13 12:52:56 +0200

    Update usuariosMasivos.ps1

--- UD7/PowerShell/usuariosMasivos.ps1
+++ UD7/PowerShell/usuariosMasivos.ps1
@@ -2,7 +2,7 @@
 $file_users=Import-Csv -Path usuarios.csv 
 foreach ($user in $file_users) { 
   $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
-  New-LocalUser $user.cuenta -Password $clave -AccountNeverExpires -PasswordNeverExpires
+  New-LocalUser $user.cuenta -Password $clave -Description $user.descripcion -AccountNeverExpires -PasswordNeverExpires
   #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
   Add-LocalGroupMember -Group usuarios -Member $user.cuenta
 }
