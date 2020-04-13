de61bbeaf803d31621d20caf413d1f05c665ec5b
Juan Carlos <jcrequena.fp@gmail.com>
2020-04-13 19:45:33 +0200

    Update pred_temperaturas.ps1

--- UD7/INM/pred_temperaturas.ps1
+++ UD7/INM/pred_temperaturas.ps1
@@ -1,12 +1,12 @@
 #Para componer el año, mes, día, hora y minuto puedes utilizar la instrucción siguiente
-#date +"%Y%m%d""%H""%M"
+#
 
 #En la variable dia, llevamos la cuenta de los pŕoximo 24, 48 y 72 horas, es decir, 1, 2 o 3.
-dia=1
+$dia=1
 #Bucle donde en cada pasada,horas vale 24, 48 o 72.
-for horas in $( seq 24 48 72); do
+ ForEach ($secuencia in 24 48 72) {
   #Componer el fichero png a descargar
-  ruta=
+  $ruta=
   powershell -command ........
-  dia=$((dia+1))
-done
+  $dia=$dia+1
+}
