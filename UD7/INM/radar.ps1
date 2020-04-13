2f7421b6f22f0861cba3085abc15bb4d59b42a75
Juan Carlos <jcrequena.fp@gmail.com>
2020-04-13 19:39:41 +0200

    Create radar.ps1

---
+++ UD7/INM/radar.ps1
@@ -0,0 +1,19 @@
+#Variables globales
+zero=0
+
+#Para componer el año, mes, día, hora y minuto puedes utilizar la instrucción siguiente
+#date +"%Y%m%d""%H""%M"
+
+
+if [ $(date +"%M") -lt 30 ]; then
+  minutos=00
+else
+  minutos=30
+fi
+
+fechaResultado=$(date --date='-1 hour' +'%Y%m%d%H')$minutos
+fechaMes=$(date +"%m")
+fechaDia=$(date +"%d")
+year=$(date +"%Y")
+
+wget .....  
